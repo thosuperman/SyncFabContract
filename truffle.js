7e4052bb6df49bf08585eb27649a43e021351580
Huy Le <huy.le@saleshood.com>
2017-11-05 07:56:29 -0800

    initial commit

---
+++ truffle.js
@@ -0,0 +1,16 @@
+// require('babel-register') ({
+//   ignore: /node_modules\/(?!zeppelin-solidity)/
+// });
+// require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      // gas: 0xfffffffffff,
+      gas: 6712390,
+      network_id: "*" // Match any network id
+    }
+  }
+};

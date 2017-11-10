09686033f248c7d22f03670c63c9b96ebb41a967
Huy Le <ledinhhuy88@gmail.com>
2017-11-09 17:22:44 -0800

    test skeleton

--- contracts/IndustryCrowdsale.sol
+++ contracts/IndustryCrowdsale.sol
@@ -5,15 +5,55 @@ import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import './MFGToken.sol';
 
 contract IndustryCrowdsale is Ownable {
+  using SafeMath for uint256;
+
   MFGToken public token;
 
-  function IndustryCrowdsale(address tokenAddress) public {
+  uint256 public startTime;
+  uint256 public endTime;
+
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  uint256 public rate;
+
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+  function IndustryCrowdsale(address tokenAddress, uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet)
+    public
+  {
+    require(_startTime >= now);
+    require(_endTime >= _startTime);
+    require(_rate > 0);
+    require(_wallet != 0x0);
+
     token = MFGToken(tokenAddress);
     assert(token.owner() == owner);
+
+    startTime = _startTime;
+    endTime = _endTime;
+    rate = _rate;
+    wallet = _wallet;
   }
 
   function transfer(address _to, uint256 _value) public onlyOwner returns (bool) {
-    token.transfer(_to, _value);
+    token.transferFrom(owner, _to, _value);
     return true;
   }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() public constant returns (bool) {
+    return now > endTime;
+  }
+
 }
--- test/IndustryCrowdsale.js
+++ test/IndustryCrowdsale.js
@@ -1,3 +1,9 @@
+import ether from 'zeppelin-solidity/test/helpers/ether'
+import {advanceBlock} from 'zeppelin-solidity/test/helpers/advanceToBlock'
+import {increaseTimeTo, duration} from 'zeppelin-solidity/test/helpers/increaseTime'
+import latestTime from 'zeppelin-solidity/test/helpers/latestTime'
+import EVMThrow from 'zeppelin-solidity/test/helpers/EVMThrow'
+
 const BigNumber = web3.BigNumber
 
 const should = require('chai')
@@ -10,20 +16,47 @@ const IndustryCrowdsale = artifacts.require("./IndustryCrowdsale.sol");
 
 contract("IndustryCrowdsale", function([owner, investor, wallet, purchaser]) {
 
+  const rate = new BigNumber(1000)
+  const value = ether(42)
+
+  const expectedTokenAmount = rate.mul(value)
+
+  before(async function() {
+    //Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock()
+  })
+
   beforeEach(async function () {
-    token = await MFGToken.new();
-    crowdsale = await IndustryCrowdsale.new(token.address);
+    this.startTime = await latestTime() + duration.weeks(1);
+    this.endTime = await this.startTime + duration.weeks(1);
+    this.afterEndTime = await this.endTime + duration.seconds(1)
+
+    this.token = await MFGToken.new();
+    this.crowdsale = await IndustryCrowdsale.new(this.token.address, this.startTime, this.endTime, rate, wallet);
   })
 
   it("should have the same owner as its token's owner", async function () {
-    let tokenOwner = await token.owner();
-    let crowdsaleOwner = await crowdsale.owner();
+    let tokenOwner = await this.token.owner();
+    let crowdsaleOwner = await this.crowdsale.owner();
     tokenOwner.should.equal(crowdsaleOwner);
   })
 
+  it('should be ended only after end', async function () {
+    let ended = await this.crowdsale.hasEnded()
+    let et = await this.crowdsale.endTime()
+    ended.should.equal(false)
+    await increaseTimeTo(this.afterEndTime)
+    ended = await this.crowdsale.hasEnded()
+    ended.should.equal(true)
+  })
+
   describe("transfering tokens", function() {
-    it.skip("should transfer from the owner's account", async function() {
-      this.crowdsale
+    it("should transfer from the owner's account", async function() {
+      await this.token.approve(this.crowdsale.address, 10);
+      await this.crowdsale.transfer(investor, 10);
+      let investorBalance = await this.token.balanceOf(investor);
+      assert.equal(investorBalance.toString(), '10');
     });
   });
+
 });

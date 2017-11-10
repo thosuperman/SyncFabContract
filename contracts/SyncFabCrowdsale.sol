3d564b18f27fabc6612c41db0892339811cf5278
Huy Le <ledinhhuy88@gmail.com>
2017-11-10 00:37:02 -0800

    remove comments

--- contracts/SyncFabCrowdsale.sol
+++ contracts/SyncFabCrowdsale.sol
@@ -11,22 +11,11 @@ contract SyncFabCrowdsale is Ownable {
 
   uint256 public startTime;
   uint256 public endTime;
-
+  uint256 public rate;
   address public wallet;
 
-  // how many token units a buyer gets per wei
-  uint256 public rate;
-
-  // amount of raised money in wei
   uint256 public weiRaised;
 
-  /**
-   * event for token purchase logging
-   * @param purchaser who paid for the tokens
-   * @param beneficiary who got the tokens
-   * @param value weis paid for purchase
-   * @param amount amount of tokens purchased
-   */
   event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
   function SyncFabCrowdsale(address tokenAddress, uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet)
@@ -50,22 +39,18 @@ contract SyncFabCrowdsale is Ownable {
     token.transferFrom(owner, _to, _value);
   }
 
-  // fallback function can be used to buy tokens
   function () payable {
     buyTokens(msg.sender);
   }
 
-  // low level token purchase function
   function buyTokens(address beneficiary) public payable {
     require(beneficiary != 0x0);
     require(validPurchase());
 
     uint256 weiAmount = msg.value;
 
-    // calculate token amount to be created
     uint256 tokens = weiAmount.mul(rate);
 
-    // update state
     weiRaised = weiRaised.add(weiAmount);
 
     transfer(beneficiary, tokens);
@@ -74,20 +59,16 @@ contract SyncFabCrowdsale is Ownable {
     forwardFunds();
   }
 
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
   function forwardFunds() internal {
     wallet.transfer(msg.value);
   }
 
-  // @return true if the transaction can buy tokens
   function validPurchase() internal constant returns (bool) {
     bool withinPeriod = now >= startTime && now <= endTime;
     bool nonZeroPurchase = msg.value != 0;
     return withinPeriod && nonZeroPurchase;
   }
 
-  // @return true if crowdsale event has ended
   function hasEnded() public constant returns (bool) {
     return now > endTime;
   }

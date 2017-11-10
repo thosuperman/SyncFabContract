5153a65500d82cefbd85473dd5aa0b390487e6be
Huy Le <ledinhhuy88@gmail.com>
2017-11-09 17:24:15 -0800

    add configurations for babel; update dependency; add deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,14 @@ var MFGToken = artifacts.require("./MFGToken.sol");
 var IndustryCrowdsale = artifacts.require("./IndustryCrowdsale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MFGToken).then(function() {
-    deployer.deploy(IndustryCrowdsale, MFGToken.address);
-  });
+  // deployer.deploy(MFGToken).then(function() {
+  //   deployer.deploy(IndustryCrowdsale, MFGToken.address);
+  // });
+  console.log("1234132412341324");
+  // deployer.then(function() {
+  //   return MFGToken.new();
+  // }).then(function(token) {
+  //   industryCrowdsale = IndustryCrowdsale.new(token.address);
+  //   token.approve(industryCrowdsale.address, 10);
+  // });
 };
